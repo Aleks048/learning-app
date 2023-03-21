349dc847346d92c3c1fcaf6d62ab724885ed9ada
Aleks048 <ytr167@gmail.com>
2023-03-21 16:46:19 -0400

    stop the wait for build

--- python_app/outside_calls/latex_calls.py
+++ python_app/outside_calls/latex_calls.py
@@ -1,3 +1,6 @@
+import subprocess
+from threading import Thread
+
 import file_system.file_system_facade as fsm
 import _utils.logging as log
 import _utils._utils_main as _u
@@ -7,34 +10,34 @@ import outside_calls.fsApp_calls as fsc
 import tex_file.tex_file_facade as tff
 
 
-
-import subprocess
-
 class MacLatex:
     def buildPDF(bookpath, subsectionWPrefix):
-        tff.Wr.TexFilePopulate.populateMainFile(subsectionWPrefix, bookpath)
+        def __build():
+            tff.Wr.TexFilePopulate.populateMainFile(subsectionWPrefix, bookpath)
 
-        subsectionDir = _upan.Paths.Section.getAbs(bookpath, subsectionWPrefix)
-        mainTexFilepath = _upan.Paths.TexFiles.Main.getAbs(bookpath, subsectionWPrefix)
-        cmd = "\
-    pushd {1}\n\
-        CMD=\"pdflatex  --shell-escape -synctex=1 -interaction=nonstopmode -file-line-error -output-directory={1}/_out {0}\"\n\
-        echo \"Running command:\"$CMD\n\
-        $CMD\n\
-    \n\
-    popd".format(mainTexFilepath, subsectionDir, subsectionWPrefix)
-        p = subprocess.Popen(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
-        output, err = p.communicate()
-        err = err.decode("utf-8")
-        if err != "":
-            log.autolog("While building the error occured: '{0}'.".format(err))
-        
-        log.autolog("Built subsection: '{0}'.".format(subsectionWPrefix))
-        
-        # move generated pdf
-        mainPDFFilepath = _upan.Paths.PDF.getAbs(bookpath, subsectionWPrefix)
-        outputPDF = _upan.Paths.TexFiles.Output.PDF.getAbs(bookpath, subsectionWPrefix)
-        fsc.currFilesystemApp.copyFile(outputPDF, mainPDFFilepath)
+            subsectionDir = _upan.Paths.Section.getAbs(bookpath, subsectionWPrefix)
+            mainTexFilepath = _upan.Paths.TexFiles.Main.getAbs(bookpath, subsectionWPrefix)
+            cmd = "\
+        pushd {1}\n\
+            CMD=\"pdflatex  --shell-escape -synctex=1 -interaction=nonstopmode -file-line-error -output-directory={1}/_out {0}\"\n\
+            echo \"Running command:\"$CMD\n\
+            $CMD\n\
+        \n\
+        popd".format(mainTexFilepath, subsectionDir, subsectionWPrefix)
+            p = subprocess.Popen(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
+            output, err = p.communicate()
+            err = err.decode("utf-8")
+            if err != "":
+                log.autolog("While building the error occured: '{0}'.".format(err))
+            
+            log.autolog("Built subsection: '{0}'.".format(subsectionWPrefix))
+            
+            # move generated pdf
+            mainPDFFilepath = _upan.Paths.PDF.getAbs(bookpath, subsectionWPrefix)
+            outputPDF = _upan.Paths.TexFiles.Output.PDF.getAbs(bookpath, subsectionWPrefix)
+            fsc.currFilesystemApp.copyFile(outputPDF, mainPDFFilepath)
+        t = Thread(target = __build)
+        t.start()
 
 
     @classmethod 
