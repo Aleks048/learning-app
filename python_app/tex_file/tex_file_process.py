69a3cc6d65f29ca89ea9b1a61be318ef2512e821
Aleks048 <ytr167@gmail.com>
2023-05-29 19:49:21 -0400

    bugfix. the vscode in section layout did not move to the correct position

--- python_app/tex_file/tex_file_process.py
+++ python_app/tex_file/tex_file_process.py
@@ -25,10 +25,10 @@ class TexFileProcess:
         conTexFilepath = _upan.Paths.TexFiles.Content.getAbs(bookName, subsection, imIdx)
         line = cls.__getIdxLine(imIdx, conTexFilepath)
 
-        extraLines = fsf.Data.Sec.imageTOCFileMoveLinesNumber(subsection)
+        extraLines = fsf.Data.Sec.imageContentFileMoveLinesNumber(subsection)
         
         if extraLines == _u.Token.NotDef.str_t:
-            extraLines = fsf.Data.Book.imageTOCFileMoveLinesNumber
+            extraLines = fsf.Data.Book.imageContentFileMoveLinesNumber
 
         line = str(int(line) + int(extraLines))
         return line
