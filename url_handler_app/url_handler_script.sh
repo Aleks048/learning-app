301cff34271f83d02fedc36d8e50c0cfbae2e307
Aleks048 <ytr167@gmail.com>
2024-10-28 20:29:27 -0400

    improvement. use local python for url handler

--- url_handler_app/url_handler_script.sh
+++ url_handler_app/url_handler_script.sh
@@ -1,5 +1,5 @@
 URL=$1
 # TT="/Users/ashum048/books/test.txt"
-cmd="import rpyc; c = rpyc.connect(\"localhost\", 12345).root; c.processLink('${URL}')"
-/opt/homebrew/bin/python3 -c "${cmd}" 
+cmd="import rpyc, sys; c = rpyc.connect(\"localhost\", 12345).root; c.processLink('${URL}')"
+/Users/ashum048/books/_python_local/3.9.18/bin/python3.9 -c "${cmd}"
 # &>${TT}
