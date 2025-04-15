be0675efe532af438fdacddb78503c458a6d9165
Aleks048 <ytr167@gmail.com>
2025-04-15 17:50:10 -0400

    cpp. feature. moving json calls to cpp. Working on the basic structure

--- python_app/_utils/cpp/src/json.cpp
+++ python_app/_utils/cpp/src/json.cpp
@@ -5,13 +5,34 @@
 #include "rapidjson/stringbuffer.h"
 #include "rapidjson/filewritestream.h" 
 
-#include "json.h"
 #include "utils.h"
 
-namespace data{
+#include "json.h"
 
-data::JSON_file::JSON_file(const std::string_view filepath):
+namespace data {
+
+data::JSON_file::JSON_file(const std::string filepath):
     filepath(filepath), document()
+{
+    std::string jsonAsString = fileUtils::readFile(filepath);
+    document.Parse(jsonAsString.c_str());
+}
+data::JSON_file::JSON_file(const std::string filepath,
+                           const std::string json_template):
+    filepath(filepath), document()
+{
+    document.Parse(json_template.c_str());
+}
+
+data::JSON_file::JSON_file(const JSON_file& file):
+    filepath(file.filepath), document()
+{
+    std::string jsonAsString = fileUtils::readFile(filepath);
+    document.Parse(jsonAsString.c_str());
+}
+
+data::JSON_file::JSON_file(JSON_file&& file):
+    filepath(file.filepath), document()
 {
     std::string jsonAsString = fileUtils::readFile(filepath);
     document.Parse(jsonAsString.c_str());
@@ -45,7 +66,7 @@ std::ostream& operator<<(std::ostream &os, const data::JSON_file& jf)
 }
 
 
-std::map<std::string, data::JSON_file> JSON::templates; 
+std::map<std::string, data::JSON_file> data::JSON::templates; 
 
 void data::JSON::reloadFilesFromDisk()
 {
@@ -56,8 +77,28 @@ void data::JSON::reloadFilesFromDisk()
 
 void data::JSON::saveFilesToDisk()
 {
-    // for (auto it: JSON::templates) {
-    // }
+    for (auto &[key, value]: JSON::templates) {
+        value.writeToFile();
+    }
+}
+
+void data::JSON::createFromTemplate(const std::string path, 
+                                    const std::string json_template)
+{
+    data::JSON_file file(path, json_template);
+    file.writeToFile();
+}
+
+data::JSON_file& data::JSON::readFile(const std::string filePath)
+{
+    if (JSON::templates.find(filePath) != JSON::templates.end()) {
+        data::JSON_file jf {filePath};
+        auto p = std::make_pair(filePath, jf);
+        //TODO: not ideal should change later to use the [] operator
+        data::JSON::templates.insert(p);
+    }
+
+    return data::JSON::templates.at(filePath);
 }
 
 }
--- python_app/_utils/cpp/src/json.h
+++ python_app/_utils/cpp/src/json.h
@@ -10,7 +10,24 @@ namespace data {
 
 class JSON_file {
 public:
-    JSON_file(const std::string_view filepath);
+    JSON_file(const std::string filepath);
+    JSON_file(const std::string filepath, 
+              const std::string json_template);
+    JSON_file(const JSON_file& file);
+    JSON_file(JSON_file&& file);
+    ~JSON_file() = default;
+
+    JSON_file& operator=(const JSON_file& other) // copy assignment
+    {
+        // implemented as move-assignment from a temporary copy for brevity
+        // note that this prevents potential storage reuse
+        return *this = JSON_file(other);
+    }
+ 
+    JSON_file& operator=(JSON_file&& other) noexcept // move assignment
+    {
+        return *this;
+    }
 
     friend std::ostream& operator<<(std::ostream& os, const JSON_file& dt);
     void writeToFile();
@@ -24,8 +41,10 @@ class JSON {
 public:
     static void reloadFilesFromDisk();
     static void saveFilesToDisk();
-    // void readFile(std::string_view filePath);
-    // // void writeFile(std::string_view filePath);// what is the data that we bring?
+    static void createFromTemplate(std::string path, 
+                                   std::string json_template);
+    data::JSON_file& readFile(const std::string filePath);
+    // void writeFile(std::string_view filePath,);// what is the data that we bring?
     // void readProperty(std::string_view jsonFilepath, 
     //                   std::string_view propertyName);
     // template<typename T>
