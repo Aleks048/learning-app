c07572c96634a088bfce83764a2fa1b0507e8a7e
ashum <ytr167@gmail.com>
2025-06-10 16:18:37 -0400

    improvement. mark windows screenshot as no implemented

--- python_app/outside_calls/screenshot_app.py
+++ python_app/outside_calls/screenshot_app.py
@@ -1,9 +1,11 @@
-import os
+import platform
 
 import _utils._utils_main as _u
 
 class ScreenshotApp:
     def takeScreenshot(savePath):
-        _u.runCmdAndWait("screencapture -ix '{0}'".format(savePath))
-
+        if platform.system() == "Darwin":
+            _u.runCmdAndWait("screencapture -ix '{0}'".format(savePath))
+        elif platform.system() == "Windows":
+            raise NotImplementedError()
 currScreenshotApp = ScreenshotApp
