ed48a1aba3d26c5c166483d88f18503f0a45f26d
ashum <ytr167@gmail.com>
2025-08-13 20:14:25 -0400

    imperovement. win starter full filepath

--- python_app/starter_win.sh
+++ python_app/starter_win.sh
@@ -1,3 +1,3 @@
-"C:\\Users\\ashum\dev\\learning-app\\_python_local\\python.exe" main.py &
+"C:\\Users\\ashum\dev\\learning-app\\_python_local\\python.exe" "C:\\Users\\ashum\dev\\learning-app\\python_app\\main.py" &
 FOO_PID=$!
 #  /Users/ashum048/books/utils/python_app/wiki/data/kiwix-serve -a ${FOO_PID} /Users/ashum048/books/utils/python_app/wiki/data/wiki.zim
